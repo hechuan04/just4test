44ae8dc5d503792424bfda64676c7b14a39be7ac
liuzongxu110 <514634635@qq.com>
2018-06-30 16:27:17 +0800

    营销系统1

--- src/com/seven7/sbstactHW/AbstrctActHW.java
+++ src/com/seven7/sbstactHW/AbstrctActHW.java
@@ -1,4 +1,33 @@
 package com.seven7.sbstactHW;
 
 public class AbstrctActHW {
+    double original;
+    double actual;
+    boolean insider;
+    int activity;
+
+    public double activity(double original, boolean insider, int activity) {
+        if (activity == 0 && original > 10) {
+            actual = original--;
+        }
+        if (activity == 1) {
+            actual = original * 0.8;
+        }
+        if (activity == 2) {
+            System.out.println("买一赠一");
+        }
+        if (activity == 3 && insider == true) {
+            actual = original * 0.9;
+        } else {
+            actual = original;
+        }
+        return actual;
+    }
 }
+
+
+
+
+
+
+
--- src/com/seven7/sbstactHW/Client.java
+++ src/com/seven7/sbstactHW/Client.java
@@ -1,8 +1,19 @@
 package com.seven7.sbstactHW;
 
-public class Client {
+public class Client extends AbstrctActHW {
+
+
+
 
     public static void main(String[] args) {
+        AbstrctActHW abstrctActHW=new AbstrctActHW();
+        abstrctActHW.activity(20.5,false,0);
+        System.out.println("实付价格："+abstrctActHW.activity);
+
+
+
+
+
 
     }
 
